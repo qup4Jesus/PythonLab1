436472cede7831a2d043d99aba6da5e75b12b942
qup4Jesus <remret1654@gmail.com>
2026-05-11 13:45:11 +0300

    Simplify deployment logic, remove AppVeyor

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -3,7 +3,7 @@ import sys
 
 try:
     # Проверяем, что сервер работает
-    response = requests.get('http://localhost:500/', timeout = 5)
+    response = requests.get('http://localhost:5000/', timeout = 5)
     print(f"Status code: {response.status_code}")
 
     if response.status_code == 200:
